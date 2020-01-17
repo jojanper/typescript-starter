00ccf5618e5f1facac54d40486546a53015d1912
Juha Ojanperä <juha.ojanpera@gmail.com>
2020-01-17 22:14:51 +0200

    Separate vendor chunks.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const packageJson = require('./package.json');
 
@@ -10,7 +12,6 @@ const VERSION = JSON.stringify(packageJson.version).replace(/"/g, '');
 function config(env) {
     const folder = argv.folder || 'build';
     const ext = (env === 'production') ? '.min' : '';
-    const filename = `engine.${VERSION}.[contentHash]${ext}.js`;
 
     const isTest = env === 'test';
     if (env === 'test') {
@@ -24,24 +25,13 @@ function config(env) {
         entry: './src/bundle.ts',
         output: {
             path: path.resolve(__dirname, folder),
-            filename,
+            filename: `[name].[contenthash].${VERSION}${ext}.js`,
             library: 'HelloLib',
             libraryTarget: 'umd',
             umdNamedDefine: true
         },
         resolve: {
             extensions: ['.js', '.ts', '.json']
-        },
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    commons: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: 'vendors',
-                        chunks: 'all'
-                    }
-                }
-            }
         },
         module: {
             rules: [
@@ -58,6 +48,7 @@ function config(env) {
     }
 
     if (isTest) {
+        // Needed for code coverage
         configuration.module.rules.push(
             {
                 test: /\.ts$/,
@@ -69,6 +60,43 @@ function config(env) {
                 }
             }
         );
+    }
+
+    if (mode === 'production') {
+        configuration = {
+            ...configuration,
+            plugins: [
+                new CleanWebpackPlugin(),
+
+                // Create index.html automatically
+                new HtmlWebpackPlugin({
+                    title: 'TypeScript starter',
+                }),
+            ],
+            optimization: {
+                moduleIds: 'hashed',
+                runtimeChunk: 'single',
+                splitChunks: {
+                    chunks: 'all',
+                    maxInitialRequests: Infinity,
+                    minSize: 0,
+                    cacheGroups: {
+                        // All third-party libraries appear as separate vendor modules
+                        vendor: {
+                            test: /[\\/]node_modules[\\/]/,
+                            name(module) {
+                                // Get the name, e.g., node_modules/packageName/part.js
+                                // or node_modules/packageName
+                                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+                                // npm package names are URL-safe, but some servers don't like @ symbols
+                                return `${packageName.replace('@', '')}`;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     return configuration;
